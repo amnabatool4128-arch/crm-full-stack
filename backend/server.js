b3a1db5f795b610b75309e00fe8401c8196311eb
Amna Batool <amnabatool4128@gmail.com>
2026-08-15 10:44:12 +0500

    Prepare backend for Vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -60,6 +60,10 @@ app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+export default app;
